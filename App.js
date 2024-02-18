da3f5f1e2bdbaefe85da88e3bfd5865313b11c91
KhaledAlaa12 <khaledalaa1055@gmail.com>
2024-02-18 02:59:45 +0200

    add ForgotPassword and ResetPassword Screens

--- App.js
+++ App.js
@@ -3,12 +3,16 @@ import { StyleSheet } from 'react-native';
 import LoginScreen from './src/screens/LoginScreen';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import SignupScreen from './src/screens/SignupScreen';
+import ForgotPassword from './src/screens/ForgotScreen';
+import ResetPassword from './src/screens/ResetPassword';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <LoginScreen />
+      {/* <LoginScreen /> */}
       {/* <SignupScreen /> */}
+      {/* <ForgotPassword /> */}
+      <ResetPassword />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
