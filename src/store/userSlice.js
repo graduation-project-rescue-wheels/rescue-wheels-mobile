90e090fda7c5f49240c86514e3398741c3e5b06f
Youssef-S-Negm <youssef.negm2001@gmail.com>
2024-02-21 20:11:00 +0200

    storing the tokens using expo-secure-store

--- src/store/userSlice.js
+++ src/store/userSlice.js
@@ -1,20 +1,28 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { signIn } from "../api/user";
 import showToast from "../components/Toast";
+import * as SecureStore from 'expo-secure-store'
 
 export const signInAsync = createAsyncThunk('user/signInAsync', async ({ email, passowrd }) => {
     const response = await signIn(undefined, email, passowrd)
 
     if (response.status === 200) {
+        await SecureStore.setItemAsync('accessToken', response.data.Token)
         showToast('Welcome to rescue wheels')
+
         return response.data
     } else {
         showToast('Something went wrong. Please try again later.')
+
         return {
             userData: null,
             accessToken: null
         }
     }
+})
+
+export const signOutAsync = createAsyncThunk('user/signOutAsync', async () => {
+    await SecureStore.deleteItemAsync('accessToken')
 })
 
 const userSlice = createSlice({
@@ -23,19 +31,17 @@ const userSlice = createSlice({
         user: null,
         accessToken: null
     },
-    reducers: {
-        signOut: (state) => {
-            state.accessToken = null
-            state.user = null
-        }
-    },
     extraReducers: builder => {
         builder.addCase(signInAsync.fulfilled, (state, action) => {
             state.user = action.payload.userData
-            state.accessToken = action.payload.Token
+            state.accessToken = action.payload.token
+        })
+
+        builder.addCase(signOutAsync.fulfilled, (state) => {
+            state.user = null
+            state.accessToken = null
         })
     }
 })
 
 export default userSlice.reducer
-export const { signOut } = userSlice.actions
