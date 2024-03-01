0bfab7fc539e80f64e9e3203b44da0808ca7feb0
Youssef-S-Negm <youssef.negm2001@gmail.com>
2024-03-01 18:33:00 +0200

    added user services flatlist item

---
+++ src/components/UserServicesFlatListItem.jsx
@@ -0,0 +1,43 @@
+import { Image, StyleSheet, TouchableOpacity, View } from 'react-native'
+import PoppinsText from './PoppinsText'
+
+const UserServicesFlatListItem = ({ imageSrc, label, onPress }) => {
+    return (
+        <TouchableOpacity
+            style={styles.container}
+            onPress={onPress}
+        >
+            <View style={styles.imageView}>
+                <Image
+                    style={styles.image}
+                    source={imageSrc}
+                />
+            </View>
+            <PoppinsText style={styles.label}>{label}</PoppinsText>
+        </TouchableOpacity>
+    )
+}
+
+export default UserServicesFlatListItem
+
+const styles = StyleSheet.create({
+    container: {
+        margin: 8,
+        alignItems: 'center',
+        padding: 8
+    },
+    image: {
+        height: 50,
+        width: 50,
+        borderRadius: 50,
+    },
+    label: {
+        fontSize: 12
+    },
+    imageView: {
+        elevation: 10,
+        borderRadius: 50,
+        padding: 4,
+        backgroundColor: 'white'
+    }
+})
