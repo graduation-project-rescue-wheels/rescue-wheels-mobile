556e7e614e720fae02db4fa9a9d4136df8b57119
Youssef-S-Negm <youssef.negm2001@gmail.com>
2024-03-07 00:23:06 +0200

    -removed date of birth argument from sign up function
    added:
    -updateUser function
    -deleteUser function

--- src/api/user.js
+++ src/api/user.js
@@ -1,4 +1,5 @@
 import rwClient from "./axios";
+import * as SecureStore from 'expo-secure-store'
 
 export async function signIn(username, email, password) {
     return await rwClient.post('/user/signIn', {
@@ -8,12 +9,36 @@ export async function signIn(username, email, password) {
     })
 }
 
-export async function signUp(firstName, lastName, email, password, mobileNumber, DOB) {
+export async function signUp(firstName, lastName, email, password, mobileNumber) {
     return await rwClient.post('/user/signUp', {
         firstName,
         lastName,
         email,
         password,
         mobileNumber
+    })
+}
+
+export async function updateUser(firstName, lastName, mobileNumber) {
+    const accessToken = await SecureStore.getItemAsync('accessToken')
+
+    return await rwClient.put('/user/UpdateUserData', {
+        firstName,
+        lastName,
+        mobileNumber,
+    }, {
+        headers: {
+            accesstoken: process.env.EXPO_PUBLIC_ACCESS_TOKEN_PREFIX + accessToken
+        }
+    })
+}
+
+export async function deleteUser() {
+    const accessToken = await SecureStore.getItemAsync('accessToken')
+    
+    return await rwClient.delete('/user/DeleteUser', {
+        headers: {
+            accesstoken: process.env.EXPO_PUBLIC_ACCESS_TOKEN_PREFIX + accessToken
+        }
     })
 }
