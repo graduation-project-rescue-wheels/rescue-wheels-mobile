0a2974bb486140e3418dae7c151019f42d609215
Youssef-S-Negm <youssef.negm2001@gmail.com>
2024-03-11 17:56:04 +0200

    added validations for vehicle inputs

--- src/utils/inputValidations.js
+++ src/utils/inputValidations.js
@@ -117,3 +117,73 @@ export const validateOldPassword = (oldPassword) => {
         }
     }
 }
+
+export const validateVehicleMaker = (make) => {
+    if (make.length === 0) {
+        return {
+            isValid: false,
+            message: 'Please enter vehicle maker'
+        }
+    } else {
+        return {
+            isValid: true,
+            message: ''
+        }
+    }
+}
+
+export const validateVehicleModel = (model) => {
+    if (model.length === 0) {
+        return {
+            isValid: false,
+            message: 'Please enter vehicle model'
+        }
+    } else {
+        return {
+            isValid: true,
+            message: ''
+        }
+    }
+}
+
+export const validateLicensePlate = (licensePlate) => {
+    if (licensePlate.length === 0) {
+        return {
+            isValid: false,
+            message: 'Please enter license plate'
+        }
+    } else {
+        return {
+            isValid: true,
+            message: ''
+        }
+    }
+}
+
+export const validateVehicleType = (type) => {
+    if (type.length === 0) {
+        return {
+            isValid: false,
+            message: 'Please enter vehicle type'
+        }
+    } else {
+        return {
+            isValid: true,
+            message: ''
+        }
+    }
+}
+
+export const validateVehicleEnergySource = (energySource) => {
+    if (energySource.length === 0) {
+        return {
+            isValid: false,
+            message: "Please enter vehicle's energy source"
+        }
+    } else {
+        return {
+            isValid: true,
+            message: ''
+        }
+    }
+}
