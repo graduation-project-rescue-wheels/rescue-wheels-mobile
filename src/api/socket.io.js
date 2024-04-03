53c7055e79dcdc1970712a75877d67ebad9bafe2
Youssef-S-Negm <youssef.negm2001@gmail.com>
2024-04-03 15:51:40 +0200

    passing token to socket auth

--- src/api/socket.io.js
+++ src/api/socket.io.js
@@ -1,5 +1,9 @@
 import { io } from 'socket.io-client'
+import { accessToken } from '../jwt/token';
 
 export const socket = io(process.env.EXPO_PUBLIC_API_URL, {
-    autoConnect: false
+    autoConnect: false,
+    auth: {
+        token: process.env.EXPO_PUBLIC_ACCESS_TOKEN_PREFIX + accessToken
+    }
 })
