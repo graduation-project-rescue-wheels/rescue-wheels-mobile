ff9b4f321253fadde6f9320e2cc87404663ff197
Youssef-S-Negm <youssef.negm2001@gmail.com>
2024-04-08 01:49:45 +0200

    added a comment explaining why auth.token is intialized to null in the socket instance

--- src/api/socket.io.js
+++ src/api/socket.io.js
@@ -3,6 +3,6 @@ import { io } from 'socket.io-client'
 export const socket = io(process.env.EXPO_PUBLIC_API_URL, {
     autoConnect: false,
     auth: {
-        token: null
+        token: null // intializing token with a null value to be able to modify it after successful sign in/user datat loading  
     }
 })
