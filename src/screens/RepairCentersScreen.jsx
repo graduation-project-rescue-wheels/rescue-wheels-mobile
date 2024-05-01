fb819588484b69b6ca8518536b7c79b6ce9a62ef
Jhany01 <johnhany07@gmail.com>
2024-05-02 00:39:40 +0300

    Added search component

--- src/screens/RepairCentersScreen.jsx
+++ src/screens/RepairCentersScreen.jsx
@@ -2,23 +2,59 @@ import { useEffect, useState } from 'react'
 import { FlatList, StyleSheet, View } from 'react-native'
 import { getAllRepairCenters } from '../api/repairCenter'
 import RepairCenterFlatListItem from '../components/RepairCenterFlatListItem'
+import CustomTextInput from '../components/CustomTextInput'
+import { Feather } from '@expo/vector-icons';
 
 const RepairCentersScreen = ({ navigation }) => {
     const [repairCenters, setRepairCenters] = useState([])
+    const [searchQuery, setsearchQuery] = useState({
+        value: '',
+        isFocused: false
+    })
+    const [filteredRCs, setfilteredRCs] = useState([])
+
 
     const fetchRepairCenters = async () => {
         const rcs = (await getAllRepairCenters()).data.data
         setRepairCenters(rcs)
+        setfilteredRCs(rcs)
+    }
+
+    const handleSearch = () => {
+        if (searchQuery.value.length > 0) {
+            setfilteredRCs(repairCenters.filter(rc =>
+                rc.name.toLowerCase().includes(searchQuery.value.toLowerCase())
+            ))
+        } else setfilteredRCs(repairCenters)
     }
 
     useEffect(() => {
         fetchRepairCenters()
     }, [])
 
+    useEffect(() => {
+        handleSearch()
+
+    }, [searchQuery.value])
+
+
     return (
         <View style={styles.constainer}>
+            <CustomTextInput
+                Icon={() => <Feather
+                    name="search"
+                    size={24}
+                    color={searchQuery.isFocused ? '#E48700' : '#adadad'} />}
+                onBlur={() => setsearchQuery({ ...searchQuery, isFocused: false })}
+                onFocus={() => setsearchQuery({ ...searchQuery, isFocused: true })}
+                onChangeText={e => setsearchQuery({ ...searchQuery, value: e })}
+                placeholder='Search for repair centers'
+                state={searchQuery}
+            >
+
+            </CustomTextInput>
             <FlatList
-                data={repairCenters}
+                data={filteredRCs}
                 renderItem={({ item }) => <RepairCenterFlatListItem item={item} navigation={navigation} />}
             />
         </View>
