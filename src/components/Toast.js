28b81b420eaee1759b47c222969fdf061d831bc1
Youssef-S-Negm <youssef.negm2001@gmail.com>
2024-05-13 00:26:16 +0300

    added a constant message

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -2,6 +2,7 @@ import { StyleSheet } from "react-native"
 import Toast from "react-native-root-toast"
 
 export const SMTH_WENT_WRONG = 'Something went wrong. Please try again later'
+export const LOCATION_PERMISSION_DENIED = "Location permission has been rejected. Can't perform the operation"
 
 const showToast = (message) => {
     Toast.show(message, {
