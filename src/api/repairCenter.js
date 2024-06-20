2be3558c358d81bc0316f4e1f21dea368627017e
Jhany01 <johnhany07@gmail.com>
2024-06-20 20:14:25 +0300

    modified getAllRepairCenters api

--- src/api/repairCenter.js
+++ src/api/repairCenter.js
@@ -1,7 +1,7 @@
 import rwClient from "./axios";
 
-export async function getAllRepairCenters() {
-    return await rwClient.get('/RepairCenter/GetAllRepairCenters')
+export async function getAllRepairCenters(filters, sortedBy, isAscending, coords) {
+    return await rwClient.get(`/RepairCenter/GetAllRepairCenters/${JSON.stringify(filters)}/${sortedBy}/${isAscending}/${JSON.stringify(coords)}`)
 }
 
 export async function getRepairCenterById(id) {
