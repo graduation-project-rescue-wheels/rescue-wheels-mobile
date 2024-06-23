cad0c75dbde0b6d8449aaf15f1e44e2d9c1a8ca1
Youssef-S-Negm <youssef.negm2001@gmail.com>
2024-06-23 21:02:30 +0300

    added ReservationHistoryFlatlistItem

---
+++ src/components/ReservationHistoryFlatListItem.jsx
@@ -0,0 +1,42 @@
+import { Image, StyleSheet, Text, View } from 'react-native'
+import PoppinsText from './PoppinsText';
+import { useMemo } from 'react';
+import { mainColor } from '../colors';
+
+const ReservationHistoryFlatListItem = ({ item }) => {
+    const date = useMemo(() => new Date(Date.parse(item.startDate)).toLocaleString(), [])
+
+    return (
+        <View style={styles.container}>
+            <Image
+                source={item.RepairCenterId.Image?.secure_url ? { uri: item.RepairCenterId.Image?.secure_url } : require('../assets/images/RCAvatar.png')}
+                style={{ width: 80, height: 80, marginRight: 8 }}
+                resizeMode='cover'
+            />
+            <View>
+                <PoppinsText style={{ color: mainColor }}>{item.title}</PoppinsText>
+                <PoppinsText style={styles.description}>{item.description}</PoppinsText>
+                <PoppinsText style={styles.description}>{date}</PoppinsText>
+            </View>
+        </View>
+    )
+}
+
+export default ReservationHistoryFlatListItem
+
+const styles = StyleSheet.create({
+    container: {
+        elevation: 5,
+        borderRadius: 16,
+        backgroundColor: 'white',
+        margin: 8,
+        paddingRight: 8,
+        flexDirection: 'row',
+        alignItems: 'center',
+        overflow: 'hidden'
+    },
+    description: {
+        color: '#ADADAD',
+        fontSize: 12
+    }
+})
